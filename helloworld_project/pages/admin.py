4e545602603cdabd530451f793ad598c7daef198
UzielM <uzielmoreno18@gmail.com>
2020-11-18 19:29:04 -0600

    Adicion de modelos para la BD

--- helloworld_project/pages/admin.py
+++ helloworld_project/pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Autor, Publicacion
 
 # Register your models here.
+admin.site.register(Autor)
+admin.site.register(Publicacion)
--- helloworld_project/pages/models.py
+++ helloworld_project/pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+#Modelo de autor
+class Autor(models.Model):
+    nombre=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+
+#Modelo de publicaciones
+class Publicacion(models.Model):
+    titulo=models.CharField(max_length=50)
+    contenido=models.CharField(max_length=250)
+    fecha_creacion=models.DateField()
+    autor=models.ForeignKey(Autor,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
